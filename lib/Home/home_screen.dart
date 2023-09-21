a17c06dc46e3845ef6abea70d41ee26a052416cb
Mohamed <mohamed.osama200538@gmail.com>
2023-09-21 11:19:15 +0300

    finished the stories design in home page

--- lib/Home/home_screen.dart
+++ lib/Home/home_screen.dart
@@ -5,6 +5,73 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 155,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal, // Horizontal scrolling
+              itemBuilder: story,
+              itemCount: 10, // Number of stories
+              padding: const EdgeInsets.all(5),
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
+
+Widget story (BuildContext context, int index){
+  return Padding(
+    padding: const EdgeInsets.only(right: 5),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        height: 150,
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/facebookStory.jpg",
+              height: 150,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                 CircleAvatar(
+                   radius: 17,
+                   backgroundColor: Colors.blue,
+                   child: Icon(
+                     Icons.person,
+                     color: Colors.white,
+                     size: 20,
+                   ),
+                 ),
+                 Padding(
+                   padding: EdgeInsets.only(left: 22),
+                   child: Text("Owner",
+                    style: TextStyle
+                      (
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18
+                    ),
+                ),
+                 ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
